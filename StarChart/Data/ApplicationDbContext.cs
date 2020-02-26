4828f46f2f6c19eb5615a7b6183a5c7b04b4b555
pranavs <Pranav.Desu@oneadvanced.com>
2020-02-26 14:58:07 +0530

    2

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -2,8 +2,12 @@
 
 namespace StarChart.Data
 {
+    using Models;
+
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
